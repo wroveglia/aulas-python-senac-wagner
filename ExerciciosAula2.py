101f19f0d83ff35094d5d32237ffac12af20962b
Wagner <wagner.roveglia@gmail.com>
2026-06-26 21:47:17 -0300

    Exercicios Aula 3 a concluir -Cassio corrigindo

---
+++ ExerciciosAula2.py
@@ -0,0 +1,49 @@
+# EXERCICIO 1
+
+'''nome = input('Digite o nome do Aluno: ')
+idade = input('Digite sua idade: ')
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print('Aprovado')
+
+elif media >= 5:
+    print('Reprovado')
+
+else:
+    print('Reprovado')
+
+print(f'Com esta media do Aluno(a) {nome} com {idade} anos de idade é {media}')'''
+
+# EXERCICIO 2
+
+arroz = 24.90
+feijão = 8.5
+macarrão = 4.30
+molho = 3.70
+óleo = 8.90
+
+
+comprararroz = input('Deseja comprar Arroz?: ')
+if comprararroz == 'sim':
+    print('arroz')
+
+else:
+    print('Não quero arroz')
+
+
+comprarfeijao= input('Deseja comprar Feijão?: ')
+if comprarfeijao == 'sim':
+    print('feijão')
+
+else:
+    print('Não quero Feijão')
+
+    
+
+    
+
+   
